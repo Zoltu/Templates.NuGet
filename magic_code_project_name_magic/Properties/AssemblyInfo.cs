1639785d8b0281497aa85b374bfac5c69f9eb19b
Micah <micah@zoltu.net>
2015-01-04 11:55:40 -0800

    Fixes company and product name in AssemblyInfo.

--- magic_code_project_name_magic/Properties/AssemblyInfo.cs
+++ magic_code_project_name_magic/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 [assembly: AssemblyTitle("magic_code_project_title_magic")]
 [assembly: AssemblyDescription("magic_code_project_description_magic")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("magic_company_magic")]
-[assembly: AssemblyProduct("magic_product_magic")]
+[assembly: AssemblyCompany("magic_company_name_magic")]
+[assembly: AssemblyProduct("magic_product_name_magic")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
