556360da2336d3833cfa7b368e6be61a82af501f
Micah <micah@zoltu.net>
2015-01-04 11:58:07 -0800

    Renames tests assembly title to match project name.

--- magic_tests_project_name_magic/Properties/AssemblyInfo.cs
+++ magic_tests_project_name_magic/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
-[assembly: AssemblyTitle("magic_tests_project_title_magic")]
-[assembly: AssemblyDescription("magic_tests_project_description_magic")]
+[assembly: AssemblyTitle("magic_tests_project_name_magic")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("magic_company_name_magic")]
 [assembly: AssemblyProduct("magic_product_name_magic")]
